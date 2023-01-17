a092950b95235d50fe81a25f127ffdb78d9fd7fc
RiumeCarlosA <riumecarlos@hotmail.com>
2023-01-17 13:38:02 +0000

    html, css e ts do tecnico

--- src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
+++ src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { Tecnico } from 'src/app/models/tecnico';
 
 @Component({
   selector: 'app-tecnico-list',
@@ -7,4 +10,25 @@ import { Component } from '@angular/core';
 })
 export class TecnicoListComponent {
 
+  ELEMENT_DATA: Tecnico[] = [
+    {
+      id: 1,
+      nome: 'Riume Carlos',
+      cpf: '140.946.719-86',
+      email: 'riumecarlos@gmail.com',
+      senha: '123',
+      perfis: ['0'],
+      dataCriacao: '15/08/2003'
+    }
+  ]
+
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol', 'acoes'];
+  dataSource = new MatTableDataSource<Tecnico>(this.ELEMENT_DATA);
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
 }
+

02dbe945e5ffea4bd23f02f225e84591c88be72a
RiumeCarlosA <riumecarlos@hotmail.com>
2023-01-22 19:48:43 +0000

    Criando componente e configurando rota para TECNICO-DELETE

--- src/app/config/api.config.ts
+++ src/app/config/api.config.ts
@@ -1,6 +1,6 @@
 export const API_CONFIG = {
     //local
-    //baseUrl : 'localhost:8080'
+    //baseUrl : 'http://localhost:8080'
 
     //Produção pelo railway
     baseUrl : 'https://helpme-backend-production.up.railway.app'
